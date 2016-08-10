df99080ea5fd01d4777718da6158dbe7eebd8b70
dengzhirong <804999766@qq.com>
2016-08-10 14:16:13 +0800

    增加接口事件

--- slide.js
+++ slide.js
@@ -1,50 +1,110 @@
 
 var slide = {};
-slide.init = function(element) {
-    new Slide(element);
+slide.init = function(options) {
+    var newSlide = new Slide(options);
+    return newSlide;
 };
 /**
  * [Slide 生成器]
  * @param {[jquery dom]} element [slide盒子元素，jQuery DOM类型]
+ * @param {[function]} getIndex [获取当前变化的index]
+ * @param {[Boolean]} isShowPrevNextBtn [是否显示上下页切换按钮]
+ * @param {[Boolean]} getItemsLength [获取slide item数量]
  */
-function Slide(element) {
+function Slide(options) {
+    // 默认参数设置
+    var element = options.element || null;
+    var getIndex = options.getIndex || null;
+    var isShowPrevNextBtn = options.isShowPrevNextBtn || false;
+    var getItemsLength = options.getItemsLength || null;
+
+    this.a = "wwwww";
+
     var slideBox = element, // slide最外层盒子
         slideList = slideBox.find("[data-slide='list']"), // item包裹容器
         slideItems = slideBox.find("[data-slide='item']"), // items
         slideItemLength = slideItems.length, // items数量
         curIndex = 0; // 当前显示的item索引
 
-    // 生成上下切换按钮
-    var prevBtn = $("<span></span>").attr("data-slide", "prev");
-    var nextBtn = $("<span></span>").attr("data-slide", "next");
-    slideBox.append(prevBtn).append(nextBtn);
+    // 获取slide item数量
+    if(typeof getItemsLength == "function") {
+        getItemsLength(slideItemLength);
+    }
+
+    // 当显示上下页切换按钮，则初始化DOM结构和绑定事件
+    if(isShowPrevNextBtn) {
+        // 生成上下切换按钮
+        var prevBtn = $("<span></span>").attr("data-slide", "prev");
+        var nextBtn = $("<span></span>").attr("data-slide", "next");
+        slideBox.append(prevBtn).append(nextBtn);
+
+        // 上下页切换动画
+        $(slideBox).delegate('[data-slide="prev"]', "click touchstart", function(e) {
+            e.stopPropagation();
+            switchToPrev();
+        });
+        $(slideBox).delegate('[data-slide="next"]', "click touchstart", function(e) {
+            e.stopPropagation();
+            switchToNext();
+        });
+        
+        // 检查是否显示上下页按钮
+        checkPrevNextShow();
+
+        // 检查是否显示上下页按钮
+        function checkPrevNextShow() {
+            if(curIndex <= 0) {
+                slideBox.find('[data-slide="prev"]').css("display", "none");
+            } else if(curIndex >= slideItemLength - 1) {
+                slideBox.find('[data-slide="next"]').css("display", "none");
+            } else {
+                slideBox.find('[data-slide="prev"]').css("display", "block");
+                slideBox.find('[data-slide="next"]').css("display", "block");
+            }
+        }
+    }
+
+    // 初始化当前索引
+    if(typeof getIndex == "function") {
+        getIndex(curIndex);
+    }
+
+    // 跳转至某一张slide
+    this.switchIndex = function(index) {
+        console.log("returnIndex: " + index);
+        switchSlide(index);
+    };
 
     // 初始化slide的样式
     slideBox.attr("data-slide", "slide");
 
-    // 初始化slide-item的宽高
-    var slideBoxWidth = slideBox.width(),
+    // 初始化slide宽度
+    var slideBoxWidth, slideBoxHeight, slideListWidth;
+    initWidth();
+    function initWidth() {
+        // 初始化slide-item的宽高
+        slideBoxWidth = slideBox.width();
         slideBoxHeight = slideBox.height();
-    slideItems.each(function(index, element) {
-        $(element).width(slideBoxWidth);
-        $(element).height(slideBoxHeight);
-    });
+        slideItems.each(function(index, element) {
+            $(element).width(slideBoxWidth);
+            $(element).height(slideBoxHeight);
+        });
 
-    // 初始化slide-list的宽度
-    var slideListWidth = slideItemLength * slideBoxWidth;
-    slideList.width(slideListWidth);
+        // 初始化slide-list的宽度
+        slideListWidth = slideItemLength * slideBoxWidth;
+        slideList.width(slideListWidth);
+    }
 
-    // 检查是否显示上下页按钮
-    checkPrevNextShow();
-
-    // 上下页切换动画
-    $(slideBox).delegate('[data-slide="prev"]', "click touchstart", function(e) {
-        e.stopPropagation();
-        switchToPrev();
-    });
-    $(slideBox).delegate('[data-slide="next"]', "click touchstart", function(e) {
-        e.stopPropagation();
-        switchToNext();
+    // 浏览器resize时，slide看杜初始化
+    var windowResizeTimer = null;
+    $(window).on("resize", function(e) {
+        if(windowResizeTimer) {
+            clearTimeout(windowResizeTimer);
+            windowResizeTimer = null;
+        }
+        windowResizeTimer = setTimeout(function() {
+            initWidth();
+        }, 50);
     });
 
     // 手势切换动画
@@ -65,16 +125,8 @@ function Slide(element) {
         }
     });
 
-    // 检查是否显示上下页按钮
-    function checkPrevNextShow() {
-        if(curIndex <= 0) {
-            slideBox.find('[data-slide="prev"]').css("display", "none");
-        } else if(curIndex >= slideItemLength - 1) {
-            slideBox.find('[data-slide="next"]').css("display", "none");
-        } else {
-            slideBox.find('[data-slide="prev"]').css("display", "block");
-            slideBox.find('[data-slide="next"]').css("display", "block");
-        }
+    function getCurIndex() {
+        return curIndex;
     }
 
     // 切换到下一张
@@ -99,6 +151,7 @@ function Slide(element) {
 
     // 切换到当前页
     function switchToCur() {
+        curIndex = curIndex;
         switchSlide(curIndex);
     }
 
@@ -108,15 +161,18 @@ function Slide(element) {
      */
     function switchSlide(index) {
         // 水平方向的偏移量
+        curIndex = index;
         var translateX = -1 * index * slideBoxWidth;
         slideList.css({
             "left": translateX
-            /*"transform": "translateX(" + translateX + "px)",
-            "-moz-transform": "translateX(" + translateX + "px)",
-            "-o-transform": "translateX(" + translateX + "px)",
-            "-webkit-transform": "translateX(" + translateX + "px)"*/
         });
-        checkPrevNextShow();
+        if(isShowPrevNextBtn) {
+            checkPrevNextShow();
+        }
+        // 返回当前索引
+        if(typeof getIndex == "function") {
+            getIndex(index);
+        }
     }
 
     /**
